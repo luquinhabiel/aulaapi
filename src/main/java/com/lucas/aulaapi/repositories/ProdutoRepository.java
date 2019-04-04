6a4dc55ca0b5a06fe0b0dd9894507934130d654e
Aluno <aluno@eteclencois.com.br>
2019-04-04 11:09:16 -0300

    Aprendemos a Paginar

--- src/main/java/com/lucas/aulaapi/repositories/ProdutoRepository.java
+++ src/main/java/com/lucas/aulaapi/repositories/ProdutoRepository.java
@@ -1,5 +1,7 @@
 package com.lucas.aulaapi.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.lucas.aulaapi.domain.Produto;
 
 @Repository  
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	public Page<Produto> findByNomeContaining(String nome, Pageable pageable); 
 }
